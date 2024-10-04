e7f8d186cb25ec7f914a2c8bb39c827cdf11fe4d
Demian <110844013+Drakonteams@users.noreply.github.com>
2024-10-03 18:25:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector('#menu-icon'); // Corrected this line
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x'); // This toggles the icon to show an 'X'
+    navbar.classList.toggle('active'); // This shows/hides the navbar
+};
