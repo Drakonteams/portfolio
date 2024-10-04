5086d672baa79cf87a78c81b32e2ffc609df53f7
Demian <110844013+Drakonteams@users.noreply.github.com>
2024-10-03 20:43:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x'); // This toggles the icon to show an 'X'
     navbar.classList.toggle('active'); // This shows/hides the navbar
 };
+function scrollToContact() {
+    const contactSection = document.getElementById('contact');
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+}
